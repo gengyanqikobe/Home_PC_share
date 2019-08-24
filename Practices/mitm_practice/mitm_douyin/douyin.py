1f70b356c92885ca83a6b9bea4d6f2324ee73e0c
gengyanqikobe <gengyanqikobe@163.com>
2019-08-24 16:52:47 +0800

    爬取抖音练习

--- Practices/mitm_practice/mitm_douyin/douyin.py
+++ Practices/mitm_practice/mitm_douyin/douyin.py
@@ -8,7 +8,20 @@ __author__ = 'KOBE'
 import requests
 
 #文件路径
-path = "D:/douyin/"
+location = 'E:\douyin\girls\\'
 
-num = 100
+num = 1
+def response(flow):
+    global num
+    path = 'ixigua'
+    #判断url是否包含抖音视频
+    if path in flow.request.url:
+        filename = location + str(num) + '.mp4'
+        res = requests.get(flow.request.url)
+        #开始下载视频
+        with open(filename,'ab') as f:
+            f.write(res.content)
+            f.flush()
+            print('下载完成:{}'.format(filename))
+            num += 1
 
--- Practices/temp.py
+++ Practices/temp.py
@@ -1,16 +1,33 @@
 
 
 
+list_a = ['{"a2":"aaa"}','{"a3":"bbb"}','{"a1":"ccc"}']
 
 
 
+dict_a={}
+
+for i in list_a:
+    #eval对str格式可以直接转换为字典
+    dict_i = eval(i)
+    dict_a.update(dict_i)
+dict_a.update({'kkk':'afb'})
+print(dict_a)
+
+#list中sort函数的用法，没有返回值，但是会改变list本身
+list_key = list(dict_a.keys())
+list_key.sort(reverse=True)
+print("使用sort排序后:",list_key)
 
 
-from pyecharts.charts import Bar
+#根据key值排序
+#注意i取得都是key的值
+for i in sorted(dict_a):
+    print("按照key排序：",(i,dict_a[i]))
 
+#根据values排序
+for i in sorted(dict_a,key=dict_a.__getitem__):
+    print("按照values排序：",(i,dict_a[i]))
 
-bar = Bar()
-bar.add_xaxis(['一月','二月','三月'])
-bar.add_yaxis('温度',[5,10,15])
-
-bar.render()
+#输入（）里面的值就找对应的，不输入就是获得所有
+print(dict_a.__getitem__('kkk'))
