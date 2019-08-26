204c39018af2f484b4ea08573dc1c0d07fe289a1
gengyanqikobe <gengyanqikobe@163.com>
2019-08-26 09:44:28 +0800

    爬取抖音练习,wancheng

--- Practices/mitm_practice/mitm_douyin/douyin.py
+++ Practices/mitm_practice/mitm_douyin/douyin.py
@@ -8,14 +8,18 @@ __author__ = 'KOBE'
 import requests
 
 #文件路径
-location = 'E:\douyin\girls\\'
+location = 'F:\douyin\girls3\\'
 
 num = 1
+list_url =[]
 def response(flow):
-    global num
+    global num,list_url
     path = 'ixigua'
+
     #判断url是否包含抖音视频
-    if path in flow.request.url:
+    if path in flow.request.url and flow.request.url not in list_url:
+        print("这是请求的url:",flow.request.url)
+
         filename = location + str(num) + '.mp4'
         res = requests.get(flow.request.url)
         #开始下载视频
@@ -24,4 +28,12 @@ def response(flow):
             f.flush()
             print('下载完成:{}'.format(filename))
             num += 1
+        list_url.append(num)
+        list_url.append(flow.request.url)
+        #print(list_url)
+
+        with open('douyin.txt','a') as h:
+            h.write(str(num)+'\n')
+            h.write(flow.request.url+'\n')
+
 
